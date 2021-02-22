8ecd42584e614aa26ed449cae445914321706e06
elena-vost <angara1298@gmail.com>
2021-02-22 20:26:24 +0100

    image od dice

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+  };
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
@@ -10,14 +13,15 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
+    let classes = `Die fas fa-dice-${
+      this.props.numberWords[this.props.val - 1]
+    }`;
     return (
-      <button
-        className={"Die"}
+      <i
+        className={classes}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
         onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+      />
     );
   }
 }
